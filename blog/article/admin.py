d53ee12609ebfc8ca52679fa808761a6f3e94b6f
zxmn147 <zxmn1476@gmail.com>
2016-11-04 11:45:28 +0800

    ch7 (not done yet)

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from article.models import Article, Comment
 
-# Register your models here.
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content']
+    list_display_links = ['article']
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']
+    
+    
+    class Meta:
+        model = Comment
+
+admin.site.register(Article)
+admin.site.register(Comment, CommentModelAdmin)
--- blog/article/models.py
+++ blog/article/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    content = models.TextField()
+    pubDateTime = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+    
+    class Meta:
+         ordering = ('-pubDateTime', )
+    
+    
+class Comment(models.Model):
+    article = models.ForeignKey(Article)
+    content = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.article.title + '-' + str(self.id)
