8ba2a3188501e393930812503e75a784d28d1724
zxmn147 <zxmn1476@gmail.com>
2016-11-11 10:03:13 +0800

    done of ch7

--- blog/article/models.py
+++ blog/article/models.py
@@ -5,18 +5,20 @@ class Article(models.Model):
     content = models.TextField()
     pubDateTime = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
+    
 
     def __str__(self):
-        return self.title
+        return self.title 
     
     
     class Meta:
-         ordering = ('-pubDateTime', )
+        ordering = ('-pubDateTime', )
     
     
 class Comment(models.Model):
     article = models.ForeignKey(Article)
     content = models.CharField(max_length=128)
+    pubDateTime = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.article.title + '-' + str(self.id)
