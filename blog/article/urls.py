7c40ea650250b1d0568f59d570aa93277b4e71f9
zxmn1476 <zxmn1476@zxmn1476-HP-Compaq-dx7510-MT>
2016-12-08 18:50:51 +0800

    9(練1）

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -3,4 +3,10 @@ from article import views
 urlpatterns = [
     url(r'^$', views.article, name='article'),
     url(r'^articleCreate/$', views.articleCreate, name='articleCreate'),
+    url(r'^articleRead/(?P<articleId>[0-9]+)/$', views.articleRead, name='articleRead'),
+    url(r'^articleUpdate/(?P<articleId>[0-9]+)/$', views.articleUpdate, name='articleUpdate'),
+    url(r'^articleDelete/(?P<articleId>[0-9]+)/$', views.articleDelete, name='articleDelete'),
+    url(r'^articleSearch/$', views.articleSearch, name='articleSearch'),
+    url(r'^commentCreate/(?P<articleId>[0-9]+)/$', views.commentCreate, name='commentCreate'),
+
 ]
--- blog/article/views.py
+++ blog/article/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.db.models.query_utils import Q
 
 
 from article.models import Article, Comment
@@ -26,7 +27,87 @@ def articleCreate(request):
         2 . If method is POST, perform form validation. Display error messages if the form is invalid
         3. Save the form to the model and redirect to the article page
     '''
-    template = 'article/articleCreate.html'
+    template = 'article/articleCreateUpdate.html'
     if request.method == 'GET':
         return render(request, template, {'articleForm':ArticleForm()})
-    
+    # POST
+    articleForm = ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+    articleForm.save()
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
+    # Or try this at the last line: return article(request)
+def articleRead(request, articleId):
+    '''
+    Read an article
+        1. Get the "article" instance; redirect to the 404 page if not found
+        2. Render the articleRead template with article instance and its
+        associated comments
+    '''
+    articleToRead = get_object_or_404(Article, id=articleId)
+    context = {
+        'article': articleToRead,
+        'comments': Comment.objects.filter(article=articleToRead)
+    }
+    return render(request, 'article/articleRead.html', context)    
+def articleUpdate(request, articleId):
+    '''
+    Update the article instance:
+        1. Get the article to update; redirect to 404 if not found
+        2. Render a bound form if the method is GET
+        3. If the form is valid, save it to the model, otherwise render a
+            bound form with error messages
+    '''
+    articleToUpdate = get_object_or_404(Article, id=articleId)
+    template = 'article/articleCreateUpdate.html'
+    if request.method == 'GET':
+        articleForm = ArticleForm(instance=articleToUpdate)
+        return render(request, template, {'articleForm':articleForm, 'article':articleToUpdate})
+    # POST
+    articleForm = ArticleForm(request.POST, instance=articleToUpdate)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm, 'article':articleToUpdate})
+    articleForm.save()
+    messages.success(request, '文章已修改')
+    return redirect('article:articleRead', articleId=articleId)
+def articleDelete(request, articleId):
+    '''
+    Delete the article instance:
+        1. Render the article page if the method is GET
+        2. Get the article to delete; redirect to 404 if not found
+    '''
+    if request.method == 'GET':
+        return article(request)
+    # POST
+    articleToDelete = get_object_or_404(Article, id=articleId)
+    articleToDelete.delete()
+    messages.success(request, '文章已刪除')
+    return redirect('article:article')
+def articleSearch(request):
+    '''
+    Search for articles:
+        1. Get the "searchTerm" from the HTML page
+        2. Use "searchTerm" for filtering
+    '''
+    searchTerm = request.GET.get('searchTerm')
+    articles = Article.objects.filter(Q(title__icontains=searchTerm) |
+                                      Q(content__icontains=searchTerm))
+    context = {'articles':articles, 'searchTerm':searchTerm}
+    return render(request, 'article/articleSearch.html', context)
+def commentCreate(request, articleId):
+    '''
+    Read an article
+        1. Get the "article" instance; redirect to the 404 page if not found
+        2. Render the articleRead template with article instance and its
+        associated comments
+    '''
+    articleToComment = get_object_or_404(Article, id=articleId)
+    if request.method == 'GET':
+        return articleRead(request, articleToComment.id )
+    # POST
+    content = request.POST.get('comment')
+
+    if content:
+        Comment.objects.create(article=articleToComment, content=content)
+        return redirect ('article:articleRead', articleId=articleId)
